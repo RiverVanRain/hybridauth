b0fb12a6482ae9dd902b0e7cc859d17befcbd0b3
RiverVanRain <support@wzm.me>
2022-03-24 03:46:38 +0300

    Remove unused $separator in parseBirthday

--- src/Data/Parser.php
+++ src/Data/Parser.php
@@ -110,7 +110,7 @@ final class Parser
      *
      * @return array
      */
-    public function parseBirthday($birthday, $seperator)
+    public function parseBirthday($birthday)
     {
         $birthday = date_parse($birthday);
 
