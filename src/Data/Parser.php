73420b232608caa3db47cb30b0440413b30906aa
RiverVanRain <support@wzm.me>
2022-03-26 16:09:49 +0300

    Fixed critical error

--- src/Data/Parser.php
+++ src/Data/Parser.php
@@ -110,7 +110,7 @@ final class Parser
      *
      * @return array
      */
-    public function parseBirthday((string) $birthday)
+    public function parseBirthday($birthday)
     {
         $birthday = date_parse($birthday);
 
