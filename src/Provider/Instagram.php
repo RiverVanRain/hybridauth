61e2fa87df00dc4df705e83799631bd304a5ac04
Nikolai Shcherbin <hello@wzm.me>
2025-02-26 11:29:48 +0300

    Fix issues with PHP 8.4 and changes to nullable vars

--- src/Provider/Instagram.php
+++ src/Provider/Instagram.php
@@ -192,7 +192,7 @@ class Instagram extends OAuth2 implements AtomInterface
      * @throws \Hybridauth\Exception\InvalidAccessTokenException
      * @throws \Hybridauth\Exception\UnexpectedApiResponseException
      */
-    public function getUserMedia($limit = 12, $pageId = null, array $fields = null)
+    public function getUserMedia($limit = 12, $pageId = null, ?array $fields = null)
     {
         if (empty($fields)) {
             $fields = [
@@ -238,7 +238,7 @@ class Instagram extends OAuth2 implements AtomInterface
      * @throws \Hybridauth\Exception\InvalidAccessTokenException
      * @throws \Hybridauth\Exception\UnexpectedApiResponseException
      */
-    public function getMedia($mediaId, array $fields = null)
+    public function getMedia($mediaId, ?array $fields = null)
     {
         if (empty($fields)) {
             $fields = [
